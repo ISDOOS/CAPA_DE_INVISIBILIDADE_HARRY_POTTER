7cab44269a98a839b709bf4e181d27c3cae5cc0c
ISDOOS <142062407+ISDOOS@users.noreply.github.com>
2023-08-19 10:25:09 -0300

    CONTRIBUIÇÕES AO FORK
    Alterações no código, conforme Readme

--- README.md
+++ README.md
@@ -1,17 +1,26 @@
-# Harry-potter-Invisible-Cloak
-
-Hello World, we have covered 4 blogs together. Thankyou so much for the support. Today let’s learn something interesting,
-lets do some magic using computer vision. I hope you all know about Harry Potter’s ‘invisible cloak’, 
-the one he uses to become invisible. We will see how we can do the same magic trick with the help of computer vision. 
-I will code with python and use the opencv library.
-
-# Requirements
-
-* python 3.5 and above
-* opencv
+# Magic Invisibility Cloak - Fork
 
 
-Blogging website: www.machinelearningman.com
+O Magic Invisibility Cloak é um projeto inspirado pela famosa capa de invisibilidade da série de livros e filmes "Harry Potter". Este projeto colaborativo tem como objetivo criar um efeito visual divertido que simula a capa de invisibilidade, utilizando Python e a biblioteca OpenCV.
 
+## Funcionalidades
 
-YouTube Channel: https://www.youtube.com/channel/UC4Ljpn7LRUwCrTz8inAksNA?view_as=subscriber
+- Efeito de "capa da invisibilidade" em tempo real usando a câmera da webcam.
+- Escolha entre três cores diferentes (amarelo, rosa e vermelho) para aplicar o efeito de invisibilidade.
+- Gravação do vídeo resultante com o efeito aplicado.
+
+Meus complementos partiram de aperfeiçoar a documentação do código, algo que tenho estudado muito sobre, juntamente com a busca por uma melhor identificação da cor que remonta a máscara da "Capa". Também foi adicionado a opção do usuário escolher a cor que deseja aplicar para Capa, visto que se torna mais acessível sua usabilidade.
+
+## Pré-requisitos
+
+Antes de executar o projeto, certifique-se de ter as seguintes bibliotecas instaladas:
+
+- OpenCV
+- NumPy
+- IPywidgets
+
+Você pode instalar as bibliotecas necessárias usando o seguinte comando:
+
+```bash
+pip install opencv-python numpy ipywidgets
+
--- code.py
+++ code.py
@@ -1,74 +1,111 @@
-##openCV to solve Harry Potter Invisible Cloak
-##Dated 9-02-2020, Author:Gaurav Chatterjee
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
 
 
-##Import opencv(cv2), Numpy array(numpy) 
 import cv2
 import time
 import numpy as np
+import ipywidgets as widgets
+from IPython.display import display, clear_output
 
-## Preparation for writing the ouput video
+# Parâmetros ajustáveis
+FRAME_SIZE = (640, 480)
+FPS = 20
+RECORDING_DURATION_SECONDS = 20
+COUNTDOWN_SECONDS = 10
+
+# Constantes de cor para detecção
+COLORS = {
+    'AMARELO': (np.array([20, 100, 100]), np.array([30, 255, 255])),
+    'ROSA': (np.array([140, 100, 100]), np.array([160, 255, 255])),
+    'VERMELHO': (np.array([0, 100, 100]), np.array([10, 255, 255]))
+}
+
+def detect_color(frame, color_name):
+    lower_bound, upper_bound = COLORS[color_name]
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower_bound, upper_bound)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8))
+    mask = cv2.morphologyEx(mask, cv2.MORPH_DILATE, np.ones((3, 3), np.uint8))
+    return mask
+
+def apply_effects(frame, background, mask):
+    mask_inv = cv2.bitwise_not(mask)
+    masked_frame = cv2.bitwise_and(frame, frame, mask=mask_inv)
+    masked_background = cv2.bitwise_and(background, background, mask=mask)
+    final_output = cv2.addWeighted(masked_frame, 1, masked_background, 1, 0)
+    return final_output
+
+# Preparação para escrever o vídeo de saída
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi',fourcc,20.0, (640,480))
+out = cv2.VideoWriter('output.avi', fourcc, FPS, FRAME_SIZE)
 
-##reading from the webcam 
+# Inicialização da captura da webcam
 cap = cv2.VideoCapture(0)
 
-## Allow the system to sleep for 3 seconds before the webcam starts
-time.sleep(3)
-count = 0
-background = 0
+# Interface gráfica
+button_start = widgets.Button(description="Iniciar Gravação")
+button_stop = widgets.Button(description="Parar Gravação")
+color_selector = widgets.Dropdown(
+    options=['AMARELO', 'ROSA', 'VERMELHO'],
+    value='AMARELO',
+    description='Escolha a Cor:'
+)
+output = widgets.Output()
 
-## Capture the background in range of 60
-for i in range(60):
-    ret,background = cap.read()
-background = np.flip(background,axis=1)
+# Exibir os elementos na interface
+display(widgets.HBox([button_start, button_stop]), color_selector, output)
+
+recording = False
+
+def start_recording(b):
+    global recording
+    with output:
+        print("Clique em 'Iniciar Gravação' novamente para iniciar a gravação em 10 segundos.")
+        button_start.disabled = True
+        button_stop.disabled = False
+        color_name = color_selector.value
+        time.sleep(COUNTDOWN_SECONDS)
+        print(f"A gravação será iniciada em 5 segundos usando a cor {color_name}.")
+        time.sleep(5)
+        recording = True
+        count = 0
+        background = None
+        for i in range(60):
+            ret, background = cap.read()
+            background = np.flip(background, axis=1)
+        while recording and count < RECORDING_DURATION_SECONDS * FPS:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frame = np.flip(frame, axis=1)
+            mask = detect_color(frame, color_name)
+            final_output = apply_effects(frame, background, mask)
+            out.write(final_output)
+            cv2.imshow("Magic", final_output)
+            cv2.waitKey(1)
+            count += 1
+        stop_recording(None)
+
+def stop_recording(b):
+    global recording
+    recording = False
+    button_start.disabled = False
+    button_stop.disabled = True
+    print("Gravação encerrada.")
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+# Configurar os botões para chamar as funções de início e parada de gravação
+button_start.on_click(start_recording)
+button_stop.on_click(stop_recording)
 
 
-## Read every frame from the webcam, until the camera is open
-while(cap.isOpened()):
-    ret, img = cap.read()
-    if not ret:
-        break
-    count+=1
-    img = np.flip(img,axis=1)
-    
-    ## Convert the color space from BGR to HSV
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+# In[ ]:
 
-    ## Generat masks to detect red color
-    lower_red = np.array([0,120,50])
-    upper_red = np.array([10,255,255])
-    mask1 = cv2.inRange(hsv,lower_red,upper_red)
 
-    lower_red = np.array([170,120,70])
-    upper_red = np.array([180,255,255])
-    mask2 = cv2.inRange(hsv,lower_red,upper_red)
 
-    mask1 = mask1+mask2
 
-    ## Open and Dilate the mask image
-    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((3,3),np.uint8))
-    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_DILATE, np.ones((3,3),np.uint8))
- 
- 
-    ## Create an inverted mask to segment out the red color from the frame
-    mask2 = cv2.bitwise_not(mask1)
- 
- 
-    ## Segment the red color part out of the frame using bitwise and with the inverted mask
-    res1 = cv2.bitwise_and(img,img,mask=mask2)
-
-    ## Create image showing static background frame pixels only for the masked region
-    res2 = cv2.bitwise_and(background, background, mask = mask1)
- 
- 
-    ## Generating the final output and writing
-    finalOutput = cv2.addWeighted(res1,1,res2,1,0)
-    out.write(finalOutput)
-    cv2.imshow("magic",finalOutput)
-    cv2.waitKey(1)
-
-cap.release()
-out.release()
-cv2.destroyAllWindows()
